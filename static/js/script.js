e6fd179e826544a000293efb164a6f549b6b29dc
martinjohnson26 <martin.johnson26@outlook.com>
2020-12-11 14:29:26 +0000

    Add add jargons functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
  $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
+    $('select').formSelect();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
         
