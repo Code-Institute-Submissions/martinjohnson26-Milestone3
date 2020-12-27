6eb96a5b3ccdb3c6c4a99180e4834aa6833e5d46
martinjohnson26 <martin.johnson26@outlook.com>
2020-12-27 12:14:40 +0000

    add edit and delete buttons to hargons html

--- app.py
+++ app.py
@@ -31,8 +31,9 @@ def get_jargons():
     k_o = mongo.db.jargons.find({"jargon_index": "K-O"})
     p_t = mongo.db.jargons.find({"jargon_index": "P-T"})
     u_z = mongo.db.jargons.find({"jargon_index": "U-Z"})
-    return render_template('jargons.html', A_E=a_e, G_J=f_j, K_O=k_o, P_T=p_t, U_Z=u_z)
-    
+    return render_template(
+        'jargons.html', A_E=a_e, G_J=f_j, K_O=k_o, P_T=p_t, U_Z=u_z)
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -125,7 +126,7 @@ def add_jargon():
 
     jargons_index = mongo.db.jargons_index.find().sort("jargon_index", 1)
     return render_template("add_jargon.html", jargons_index=jargons_index)
-    
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
